928e892fe3357317ac63ade0a0342f01db9b425a
ladyJane1999 <69354433+ladyJane1999@users.noreply.github.com>
2022-06-23 11:17:44 +0300

    Add files via upload

---
+++ DemoShopDevice/Repository/ICommonRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DemoShopDevice.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PagedList;
+using SiteShopCar.Model;
+
+namespace ShopDevice.Repository
+{
+    public interface ICommonRepository<T> : IDisposable
+
+    {
+        PaginationDTO<T> GetEntity(int page,int limt);
+        T GetEntityByID(int? entityId);
+        void InsertEntity(T entity);
+        void DeleteEntity(int entityID);
+        void UpdateEntity(T entity);
+        void Save();
+    }
+}
